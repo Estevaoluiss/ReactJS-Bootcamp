d665528724a4f934af34ef8466ab2406983c507e
Estevão Luiz de siqueira nunes <102254216+Estevaoluiss@users.noreply.github.com>
2022-05-04 21:52:14 -0300

    test 1

--- aula1/src/index.js
+++ aula1/src/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
+function sum(a, b) {
+  return a + b
+}
+
+function primeiroJSX() {
+  return (
+    <div className="test">
+      Estevão
+      <h1>Soma : {sum(10, 20)}</h1>
+    </div>
+  )
+}
+
 const App = () => {
-  return <div className="App">hello world</div>
+  return <div className="App">{primeiroJSX()}</div>
 }
 
 const rootElement = document.getElementById('root')
