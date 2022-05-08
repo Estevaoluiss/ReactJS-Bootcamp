46557f69e984772fff354164d0e7860f5ac19220
Estevão Luiz de siqueira nunes <102254216+Estevaoluiss@users.noreply.github.com>
2022-05-08 18:54:20 -0300

    1

--- webpack/src/App.jsx
+++ webpack/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+
+const showEvent=()=>{
+  console.log('evento clicado')
+}
+
+
+const Button = <button>Mostrar Evento</button>
+
 const App = () => {
   return (
     <div>
       <p>Digital Inoovation</p>
       <p>Bem vindo a nossa aula</p>
+      {Button}
     </div>
   )
 }
